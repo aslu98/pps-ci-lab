5fde6c6f641b075406089105a6faaa683d0cc399
Asia <asia.lucchi@studio.unibo.it>
2021-04-29 16:03:13 +0200

    test

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins { 
+	java 
+	scala 
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.scala-lang:scala-library:2.12.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/test/scala/Test.scala
@@ -0,0 +1,7 @@
+class TryFunctions {
+  @Test
+  def testHelloWorld() {
+    val f: String = "Hello World";
+    assertEquals(f, "Hello World")
+  }
+}
